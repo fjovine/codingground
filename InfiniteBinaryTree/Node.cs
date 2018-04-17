3ddceb1e6b218ecee35ea1e6508db6b85748d668
Francesco Iovine <iovinemeccanica@gmail.com>
2018-04-17 19:17:09 +0200

    Created by Coding Ground Client

---
+++ InfiniteBinaryTree/Node.cs
@@ -0,0 +1,35 @@
+namespace InfiniteBinaryTree 
+{
+    class Node 
+    {
+        public ulong Number 
+        {
+            get;
+            private set;
+        }
+        
+        public double Alpha 
+        {
+            get;
+            private set;
+        }
+        
+        public double Length 
+        {
+            get;
+            private set;
+        }
+        
+        public double X
+        {
+            get;
+            private set;
+        }
+        
+        public double Y
+        {
+            get;
+            private set;
+        }
+    }
+}
